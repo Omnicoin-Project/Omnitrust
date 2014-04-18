cb43bdab5e468ee02c9b5b7ebc012a0a169bd16c
Brandon Anzaldi <brandon@brandonanzaldi.com>
2014-04-18 01:28:00 -0700

    Updating deps and initializing RPC interface

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@
  */
 
 var crypto = require('crypto');
+var rpc = require('node-json-rpc');
+var rpcOpts = {
+	port: 43555,
+	host: '127.0.0.1'
+}
 
 crypto.randomBytes(64, function(ex,buf) {
 	var cstring = buf.toString('hex');
@@ -13,5 +18,8 @@ exports.index = function(req, res){
 	if(!req.session.challengeString) {
 		req.session.challengeString = cstring;
 	}
-	res.render('index', { title: 'Express' });
+	res.render('index', { 
+		title: 'Express', 
+		cstring: req.session.challengeString 
+	});
 };
